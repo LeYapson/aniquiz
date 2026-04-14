6e225d2f40b021328be377229e03c75210ed3910
theau <theau.yapi@ynov.com>
2026-04-14 14:26:39 +0200

    first backend service in go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"github.com/LeYapson/aniquiz/internal/database"
-
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -17,5 +17,20 @@ func main() {
 		log.Fatal(err);
 	}
 	defer conn.Close(context.Background())
-	fmt.Println("✅ Connecté avec succès à la base DBngin !")
+	
+
+	// 2 - Initialisation du router Gin
+	router := gin.Default()
+
+	// 3 - Définition des routes
+	router.GET("/ping", func(c *gin.Context)  {
+		c.JSON(http.StatusOK, gin.H {
+			"message": "pong",
+			"status": "connected to DB",
+		})
+	})
+
+	// 4 - Démarrage du serveur
+	fmt.Println("Serveur lancé sur le http://localhost:8080")
+	router.Run(":8080")
 }
