116e9a650732e19a978a2646844be0496f4e39a1
BoJulien <julien.bonnet@ynov.com>
2026-06-22 14:27:22 +0200

    fix : api ulr

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   reporter: process.env.CI ? 'html' : 'list',
 
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL: 'http://192.168.27.74:5173',
     trace: 'on-first-retry',
   },
 
@@ -18,7 +18,7 @@ export default defineConfig({
 
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:5173',
+    url: 'http://192.168.27.74:5173',
     reuseExistingServer: !process.env.CI,
     timeout: 30_000,
   },
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,13 +7,13 @@ export default defineConfig({
     proxy: {
       // Redirige les appels WebSocket
       '/ws': {
-        target: 'ws://127.0.0.1:8080',
+        target: 'ws://192.168.27.74:8080',
         ws: true,
         changeOrigin: true
       },
       // Redirige les appels API classiques (si tu en as)
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://192.168.27.74:8080',
         changeOrigin: true
       }
     }
